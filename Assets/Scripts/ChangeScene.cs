93c85e34f088160f7d9f09379ff115d41a9c51a1
Tim <tbajmat@gmail.com>
2015-11-17 09:48:18 +0100

    New Menu Scene
    Github Deleted the other one :)

--- Assets/Scripts/ChangeScene.cs
+++ Assets/Scripts/ChangeScene.cs
@@ -1,18 +1,9 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-<<<<<<< HEAD
 public class ChangeScene : MonoBehaviour 
 {
 	void ChangeToScene(int sceneToChangeTo){
 		Application.LoadLevel(sceneToChangeTo);
-=======
-public class ChangeScene : MonoBehaviour {
-
-	
-	// Update is called once per frame
-	public void ChangeToScene (string toChangeTo) {
-        Application.LoadLevel(toChangeTo);
->>>>>>> origin/master
 	}
 }
