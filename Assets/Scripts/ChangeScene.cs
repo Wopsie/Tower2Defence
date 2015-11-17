7f115a9b200b201ffe570273af4650ec172acc2e
Tim <tbajmat@gmail.com>
2015-11-17 09:37:30 +0100

    Deleted some test scene stuff

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeScene : MonoBehaviour 
+{
+	void ChangeToScene(int sceneToChangeTo){
+		Application.LoadLevel(sceneToChangeTo);
+	}
+}
---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour 
+{
+
+	public Text timerText;
+	public float startTime;
+	private bool timerStop = false;
+	private int count;
+
+	// Use this for initialization
+	void Start () 
+	{
+		startTime = Time.time;
+	}
+	void OnGUI()
+	{
+		if(GUI.Button(new Rect( 180, 60, 80, 25), "Stop Timer"))
+				SendMessage("TimerStop");
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		if(timerStop){
+			return;
+		}
+
+		float time = Time.time - startTime;
+		string minutes = ((int)time / 60).ToString("00");
+		string seconds = (time % 60).ToString("00");
+
+		timerText.text = minutes + ":" + seconds;
+
+
+
+	}
+
+	public void TimerStop()
+	{
+		timerStop = true;
+		timerText.color = Color.yellow;
+		Debug.Log(timerText.text + count);
+	}
+}
